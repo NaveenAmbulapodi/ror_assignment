1bbef84e0b81c44273bd13262aa28ca59454770b
NavyaIndugu <NavyaIndugu>
2022-03-07 15:47:33 +0530

    my project

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :projects
 
   root to: "projects#index"
+  #devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
