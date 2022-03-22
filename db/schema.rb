b635391eb54177d64a91643f0fbb699919e78efb
Osipass@2395 <saijagadeesh9703@gmail.com>
2022-03-22 15:36:05 +0530

    nav-bar

--- db/schema.rb
+++ db/schema.rb
@@ -43,14 +43,6 @@ ActiveRecord::Schema.define(version: 2022_03_15_122745) do
     t.index ["role_id"], name: "index_employee_roles_on_role_id"
   end
 
-  create_table "employees", force: :cascade do |t|
-    t.string "name"
-    t.integer "role_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["role_id"], name: "index_employees_on_role_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -103,6 +95,5 @@ ActiveRecord::Schema.define(version: 2022_03_15_122745) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "employee_roles", "roles"
-  add_foreign_key "employees", "roles"
   add_foreign_key "profiles", "users"
 end
